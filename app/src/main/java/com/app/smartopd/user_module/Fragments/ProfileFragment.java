8092f2e476a2f311b9abaf17b4704cc2fdee1daa
asus <ritesheklare7@gmail.com>
2026-02-18 13:06:46 +0530

    Structural commit

--- app/src/main/java/com/app/smartopd/user_module/Fragments/ProfileFragment.java
+++ app/src/main/java/com/app/smartopd/user_module/Fragments/ProfileFragment.java
@@ -1,4 +1,4 @@
-package com.app.smartopd.user_module;
+package com.app.smartopd.user_module.Fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.app.smartopd.R;
+import com.app.smartopd.user_module.Adapters.EmptyHistoryAdapter;
 
 public class ProfileFragment extends Fragment {
 
--- app/src/main/java/com/app/smartopd/user_module/Models/UserHomeModel.java
+++ app/src/main/java/com/app/smartopd/user_module/Models/UserHomeModel.java
@@ -1,4 +1,4 @@
-package com.app.smartopd.user_module;
+package com.app.smartopd.user_module.Models;
 
 public class UserHomeModel {
 
